b56fd06f2cf5638a7c1722727bcb00bf0ed51031
KysylOleh1980 <oleh.k.arpo@gmail.com>
2021-01-01 12:02:12 +0200

    rewrite di.xml

---
+++ Traning2point3/Test/Controller/NoRouteHandler.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Traning2point3\Test\Controller;
+
+
+class NoRouteHandler implements \Magento\Framework\App\Router\NoRouteHandlerInterface
+{
+    public function process(\Magento\Framework\App\RequestInterface $request)
+    {
+        $moduleName = 'cms';
+        $controllerPath = 'index';
+        $controllerName = 'index';
+        $request->setModuleName($moduleName)
+            ->setControllerName($controllerPath)
+            ->setActionName($controllerName);
+        return true;
+    }
+}
---
+++ Traning2point3/Test/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE, 'Traning2point3_Test',
+    __DIR__
+);
+
