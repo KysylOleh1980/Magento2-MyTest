664677b2a6ab8c876c5adf7cc2e5ae63475793f2
KysylOleh1980 <oleh.k.arpo@gmail.com>
2021-01-03 22:53:45 +0200

    add resutRaw

--- Traning3point2/Test/Controller/Index/Index.php
+++ Traning3point2/Test/Controller/Index/Index.php
@@ -5,22 +5,27 @@ namespace Traning3point2\Test\Controller\Index;
 use Magento\Framework\App\Action\Action;
 use Magento\Backend\App\Action\Context;
 use Magento\Framework\View\LayoutFactory;
+use Magento\Framework\Controller\Result\RawFactory;
 
 class Index extends Action
 {
-    private $layoutFactory;
+    private $_layoutFactory;
 
-    public function __construct(Context $context, LayoutFactory $layoutFactory)
+    private $_resultRawFactory;
+
+    public function __construct(Context $context, LayoutFactory $layoutFactory, RawFactory $resultRawFactory)
     {
         parent::__construct($context);
-        $this->layoutFactory = $layoutFactory;
+        $this->_layoutFactory = $layoutFactory;
+        $this->_resultRawFactory = $resultRawFactory;
     }
 
 
     public function execute()
     {
-        $layout = $this->layoutFactory->create();
+        $layout = $this->_layoutFactory->create();
         $block = $layout->createBlock('Traning3point2\Test\Block\MyBlock');
-        $this->getResponse()->appendBody($block->toHtml());
+        $result = $this->_resultRawFactory->create();
+        return $result->setContents($block->toHtml());
     }
 }
